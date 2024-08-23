f1a42de552203a7b5307247d95709aff22013de4
yuechuye <3391855334@qq.com>
2024-08-23 16:58:53 +0800

    init

---
+++ src/main/java/com/yy/YBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YBlogApplication.class, args);
+    }
+
+}
